15c0698359cfcff959ddff40fb58aea1545bc925
Alexis Mourlanne <alexis.mourlanne@etu.u-bordeaux.fr>
2019-06-20 12:52:42 +0200

    ajout de middelwares

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,20 @@
 import express, { Request, Response } from 'express';
 import router from './routes';
+import logger from 'morgan';
+import helmet from 'helmet';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
 const app = express();
+
+app.use(logger('dev'));
+
+app.use(cors());
+app.use(helmet());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.get('/', (req: Request, res: Response) => {
   res.redirect('/api');
